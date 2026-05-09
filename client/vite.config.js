092b0b8e44453ef708e15fe7f4dbd68d28b5d845
mreducator4545-cyber <mreducator4545@gmail.com>
2026-05-09 16:40:44 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -12,5 +13,5 @@ export default defineConfig({
         autoprefixer,
       ],
     },
-  },
+  }
 })
